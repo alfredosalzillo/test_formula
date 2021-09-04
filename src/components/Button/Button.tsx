4c54322d385af8609fcc8e9d80fe3e32478dbe53
alfredo.salzillo <noctisbels@gmai.com>
2021-09-04 23:00:36 +0200

    added Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import clsx from 'clsx';
+import classes from './Button.module.css';
+
+export type ButtonProps = React.PropsWithChildren<{
+  color?: 'primary' | 'default',
+  fullWidth?: boolean,
+  disabled?: boolean,
+} & React.ButtonHTMLAttributes<HTMLButtonElement>>;
+
+const Button = (props: ButtonProps) => {
+  const {
+    color,
+    fullWidth = false,
+    disabled = false,
+    ...others
+  } = props;
+  const className = clsx(
+    classes.root,
+    {
+      [classes.fullWidth]: fullWidth,
+      [classes.disabled]: disabled,
+      [classes.primary]: color === 'primary',
+    },
+    others.className,
+  );
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      {...others}
+      className={className}
+    />
+  );
+};
+export default Button;
