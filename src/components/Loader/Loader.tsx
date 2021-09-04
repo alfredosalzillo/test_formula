677ad070602a253e245ffb3f7c336f79d4d1078b
alfredo.salzillo <noctisbels@gmai.com>
2021-09-04 23:00:56 +0200

    added Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Loader.module.css';
+
+const Loader = () => (
+  <div className={classes.root}>
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
